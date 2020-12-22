67e6517f39342b769dd46ad36891f78244114ee1
jiaopi404 <1240659804@qq.com>
2020-12-22 19:03:28 +0800

    update swagger 配置文件，通过服务的 application.yml 进行开启与关闭

---
+++ changgou-parent/changgou-common/src/main/java/com/changgou/conf/swagger/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.changgou.conf.swagger;
+
+import com.google.common.base.Predicates;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@ConditionalOnProperty(name = "swagger.enable", havingValue = "true")
+public class SwaggerConfig {
+    @Bean
+    public Docket webApiConfig () {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("webApi")
+                .apiInfo(webApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(Predicates.not(PathSelectors.regex("/admin/.*")))
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                .build();
+    }
+    private ApiInfo webApiInfo(){
+
+        return new ApiInfoBuilder()
+                .title("API文档标题")
+                .description("本文档描述了微服务、分布式Swagger2整合")
+                .version("1.0")
+                .contact(new Contact("TAO", "http://www.baidu.com", "18773019101"))
+                .build();
+    }
+
+}
