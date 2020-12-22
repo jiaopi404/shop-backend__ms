cccf7739d9e3fcf4ccf3afa49d7827ce1790f80b
jiaopi404 <1240659804@qq.com>
2020-12-22 19:04:29 +0800

    update 修改 MapperScan 的范围

--- changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/GoodsApplication.java
+++ changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/GoodsApplication.java
@@ -3,13 +3,14 @@ package com.changgou;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.stereotype.Repository;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @EnableEurekaClient // 开启 Eureka 客户端
 // 开启通用 Mapper 的包扫描
 // 注解包名: tk.mybatis.spring.annotation.MapperScan; tk 开头
-@MapperScan(basePackages = {"com.changgou.dao"})
+@MapperScan(basePackages = {"com.changgou.dao"}, annotationClass = Repository.class)
 public class GoodsApplication {
     public static void main(String[] args) {
         SpringApplication.run(GoodsApplication.class, args);
