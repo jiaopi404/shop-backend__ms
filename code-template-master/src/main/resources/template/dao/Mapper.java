ea4e4c629e3c2dcd9c15c94328a413316bc54cba
jiaopi404 <1240659804@qq.com>
2021-01-04 14:34:24 +0800

    update 添加代码生成器

---
+++ code-template-master/src/main/resources/template/dao/Mapper.java
@@ -0,0 +1,11 @@
+package ${package_mapper};
+import ${package_pojo}.${Table};
+import tk.mybatis.mapper.common.Mapper;
+
+/****
+ * @Author:jiaopi404
+ * @Description:${Table}的Dao
+ * @Date 2021/1/4 14:30
+ *****/
+public interface ${Table}Mapper extends Mapper<${Table}> {
+}
