008d37f9b3e0bd2563c21c85472b673b5f108fe2
jiaopi404 <1240659804@qq.com>
2021-01-04 14:58:59 +0800

    update 添加使用代码生成器生成的代码，包括 Mapper, Service, ServiceImpl, Controller

---
+++ changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/dao/CategoryBrandMapper.java
@@ -0,0 +1,13 @@
+package com.changgou.dao;
+import com.changgou.goods.pojo.CategoryBrand;
+import org.springframework.stereotype.Repository;
+import tk.mybatis.mapper.common.Mapper;
+
+/****
+ * @Author:jiaopi404
+ * @Description:CategoryBrand的Dao
+ * @Date 2021/1/4 14:30
+ *****/
+@Repository
+public interface CategoryBrandMapper extends Mapper<CategoryBrand> {
+}
---
+++ changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/dao/SpuMapper.java
@@ -0,0 +1,13 @@
+package com.changgou.dao;
+import com.changgou.goods.pojo.Spu;
+import org.springframework.stereotype.Repository;
+import tk.mybatis.mapper.common.Mapper;
+
+/****
+ * @Author:jiaopi404
+ * @Description:Spu的Dao
+ * @Date 2021/1/4 14:30
+ *****/
+@Repository
+public interface SpuMapper extends Mapper<Spu> {
+}
